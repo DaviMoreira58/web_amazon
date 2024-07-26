8e2481c3fc82fecfcca4ecdbd4f00899fbff2763
Davi <davimoreira58@gmail.com>
2024-07-25 22:42:09 -0300

    criando o histograma

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+import plotly.express as px
 
 st.set_page_config(layout='wide')
 source = os.path.abspath('.')
@@ -16,4 +17,7 @@ price_min = df_top100_books['book price'].min()
 
 max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
 df_books = df_top100_books[df_top100_books['book price'] <= max_price]
-df_books # apenas para mostrar em tempo real na pagina web
+df_books
+
+fig = px.bar(df_books['year of publication'].value_counts())
+st.plotly_chart(fig)
