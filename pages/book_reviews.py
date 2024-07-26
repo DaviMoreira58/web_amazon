d76c8bcbc599ccc122f6009de6184d09038f969a
Davi <davimoreira58@gmail.com>
2024-07-25 23:00:38 -0300

    filtro dos reviews por livro

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -2,6 +2,8 @@ import streamlit as st
 from main import df_reviews, df_top100_books
 import pandas as pd
 
+st.set_page_config(layout='wide')
+
 books = df_top100_books['book title'].unique()
 book = st.sidebar.selectbox(
     'Books',
@@ -10,3 +12,6 @@ book = st.sidebar.selectbox(
 
 df_book = df_top100_books[df_top100_books['book title'] == book]
 df_book # para exibir na pagina
+
+df_reviews_f = df_reviews[df_reviews['book name'] == book]
+df_reviews_f # para exibir na pagina
