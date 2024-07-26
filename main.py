36f46b5560e2d1aa17156bc332dbb6dbfd506687
Davi <davimoreira58@gmail.com>
2024-07-25 22:32:08 -0300

    primeiro componente price slider

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ path_book = os.path.join(dir, 'Top-100 Trending Books.csv')
 df_reviews = pd.read_csv(path_reviews)
 df_top100_books = pd.read_csv(path_book)
 
-df_reviews # apenas para mostrar a tabela no navegador
+price_max = df_top100_books['book price'].max()
+price_min = df_top100_books['book price'].min()
+st.slider('Price Range', price_min, price_max, price_max)
