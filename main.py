fa308231024d4602b6ede6e794a0853c356a34ef
Davi <davimoreira58@gmail.com>
2024-07-25 21:09:55 -0300

    configurando e verificando tabela no browser

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
+st.set_page_config(layout='wide')
 source = os.path.abspath('.')
 dir = os.path.join(source, 'datasets')
 path_reviews = os.path.join(dir, 'customer reviews.csv')
@@ -10,3 +11,4 @@ path_book = os.path.join(dir, 'Top-100 Trending Books.csv')
 df_reviews = pd.read_csv(path_reviews)
 df_top100_books = pd.read_csv(path_book)
 
+df_reviews # apenas para mostrar a tabela no navegador
