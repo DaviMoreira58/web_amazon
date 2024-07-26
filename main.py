237d8692ab8cf07dbc0bffc93935098e13dc3457
Davi <davimoreira58@gmail.com>
2024-07-25 21:06:24 -0300

    criando os caminhos e objetos de leitura

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import os
 
-st.write('Ola, mundo') # apenas para veficar o funcionamento
+source = os.path.abspath('.')
+dir = os.path.join(source, 'datasets')
+path_reviews = os.path.join(dir, 'customer reviews.csv')
+path_book = os.path.join(dir, 'Top-100 Trending Books.csv')
+
+df_reviews = pd.read_csv(path_reviews)
+df_top100_books = pd.read_csv(path_book)
+
